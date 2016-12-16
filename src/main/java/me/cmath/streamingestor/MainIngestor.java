9303f2a50197a23ece8b53d24d1bdf82d43b26d6
Miguel Algorri <algorrim@gmail.com>
2016-12-16 13:03:08 +0100

    Updated Christian's Stream Ingestor to work with Kafka

--- src/main/java/me/cmath/streamingestor/MainIngestor.java
+++ src/main/java/me/cmath/streamingestor/MainIngestor.java
@@ -18,16 +18,17 @@ import org.apache.commons.cli.Options;
 public class MainIngestor {
 
   public static final String APP_NAME = "StreamIngestor";
-  private int _serverPort = 18000;
+  private String _kafkaServer = "localhost:9093";
   private int _throughPut = 10000;
   private int _duration = 3000;
   private int _maxTupels = -1;
   private String _dataSourceFile = "";
-  private ServerSocket _listenSocket;
   private AtomicInteger _consumedTuples;
+  private String _topic;
 
-  public MainIngestor(int serverPort, int throughPut, int duration, int maxTupels, String dataSourceFile) {
-    _serverPort = (serverPort == 0) ? _serverPort : serverPort;
+  public MainIngestor(String kafkaServer, int throughPut, int duration, int maxTupels, String dataSourceFile, String topic) {
+    _kafkaServer = (kafkaServer.equals("")) ? _kafkaServer : kafkaServer;
+    _topic = (topic.equals("")) ? _topic : topic;
     _throughPut = (throughPut == 0) ? _throughPut : throughPut;
     _duration = (duration == 0) ? _duration : duration;
     _dataSourceFile = dataSourceFile;
@@ -35,30 +36,17 @@ public class MainIngestor {
     _maxTupels = maxTupels;
   }
 
-  public void startServer() {
+  public void startStream() {
     try {
-      _listenSocket = new ServerSocket(_serverPort);
       BufferedReader dataSource = new BufferedReader(new FileReader(new File(_dataSourceFile)));
       RateLimiter rateLimiter = RateLimiter.create(_throughPut);
-      long startTime = 0;
-      System.out.println("Server started on port " + _serverPort + "..");
-      while (true) {
-        if (_listenSocket.isClosed()) {
+      long startTime = System.currentTimeMillis();
+      StreamServer c = new StreamServer(_kafkaServer, rateLimiter, startTime, _duration, dataSource, _consumedTuples, _maxTupels, _topic);
+      if (System.currentTimeMillis() - startTime > _duration + 3000) {
           printStats();
-          break;
-        }
-        Socket clientSocket = _listenSocket.accept();
-        if (startTime == 0) {
-          startTime = System.currentTimeMillis();
-        }
-        StreamServer c = new StreamServer(clientSocket, rateLimiter, startTime, _duration, dataSource, _consumedTuples, _maxTupels);
-        if (System.currentTimeMillis() - startTime > _duration + 3000) {
-          printStats();
-          break;
-        }
       }
     } catch (IOException e) {
-      System.out.println("Error: " + e.getMessage() + " (using localhost:" + _serverPort + ")");
+      System.out.println("Error: " + e.getMessage());
     } catch (Exception e) {
       System.out.println("Shutting down due to interruption..");
     }
@@ -71,27 +59,19 @@ public class MainIngestor {
     System.out.println("Actual throughput: " + actualTP + " tuples/sec");
   }
 
-  public void stopServer() {
-    try {
-      printStats();
-      _listenSocket.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
   public static void main(String[] args) {
-    int serverPort = 0, throughPut = 0, duration = 0, maxTuples = -1;
-    String dataSourceFile = null;
+    int throughPut = 0, duration = 0, maxTuples = -1;
+    String dataSourceFile = null, kafkaServer = null, topic = null;
 
     Options options = new Options();
 
     options.addOption("h", "help", false, "Show this dialog");
-    options.addOption("p", "port", true, "The port that the server runs on");
+    options.addOption("p", "port", true, "The kafka cluster brokers addresses and ports <address:port,address:port...>");
     options.addOption("t", "throughput", true, "The amount of tuples per second that will be streamed");
     options.addOption("d", "duration", true, "The amount of time in ms that tuples will be streamed");
     options.addOption("f", "file", true, "The input file with tuples separated by line breaks");
     options.addOption("m", "maxtuples", true, "Max tuples to read from file. This will override any time contraints and close the stream when all tuples have been streamed.");
+    options.addOption("T", "topic", true, "Kafka topic to push the data to");
 
     CommandLineParser parser = new BasicParser();
     HelpFormatter formater = new HelpFormatter();
@@ -108,7 +88,7 @@ public class MainIngestor {
         maxTuples = Integer.parseInt(cmd.getOptionValue("m"));
       }
       if (cmd.hasOption("p")) {
-        serverPort = Integer.parseInt(cmd.getOptionValue("p"));
+        kafkaServer = cmd.getOptionValue("p");
       }
       if (cmd.hasOption("t")) {
         throughPut = Integer.parseInt(cmd.getOptionValue("t"));
@@ -116,10 +96,16 @@ public class MainIngestor {
       if (cmd.hasOption("d")) {
         duration = Integer.parseInt(cmd.getOptionValue("d"));
       }
+      if (cmd.hasOption("T")){
+        topic = cmd.getOptionValue("T");
+      } else {
+        System.err.println("Please specify a topic");
+        System.exit(1);
+      }
       if (cmd.hasOption("f")) {
         dataSourceFile = cmd.getOptionValue("f");
       } else {
-        System.err.println("Please specifiy an input file");
+        System.err.println("Please specify an input file");
         System.exit(1);
       }
     } catch (Exception e) {
@@ -127,14 +113,14 @@ public class MainIngestor {
       System.exit(0);
     }
 
-    final MainIngestor mainIngestor = new MainIngestor(serverPort, throughPut, duration, maxTuples, dataSourceFile);
+    final MainIngestor mainIngestor = new MainIngestor(kafkaServer, throughPut, duration, maxTuples, dataSourceFile, topic);
     Runtime.getRuntime().addShutdownHook(new Thread() {
       @Override
       public void run() {
         mainIngestor.printStats();
       }
     });
-    mainIngestor.startServer();
+    mainIngestor.startStream();
 
 
   }
--- src/main/java/me/cmath/streamingestor/StreamServer.java
+++ src/main/java/me/cmath/streamingestor/StreamServer.java
@@ -1,11 +1,17 @@
 package me.cmath.streamingestor;
 
 import com.google.common.util.concurrent.RateLimiter;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.EOFException;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Properties;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
@@ -14,39 +20,45 @@ public class StreamServer extends Thread {
   private int _duration;
   private BufferedReader _sourceBuffer;
   private BufferedOutputStream _output;
-  private Socket _clientSocket;
+  private String _kafkaServer;
   private RateLimiter _rateLimiter;
   private final int END_OF_STREAM_SIG = 0;
   private long _startTime;
   private AtomicInteger _cosumedTuples;
+  private String _topic;
 
-  public StreamServer(Socket aClientSocket, RateLimiter rateLimiter, long startTime, int duration,
-      BufferedReader dataSource, AtomicInteger consumedTuples, int maxTupels) {
+  public StreamServer(String kafkaServer, RateLimiter rateLimiter, long startTime, int duration,
+      BufferedReader dataSource, AtomicInteger consumedTuples, int maxTupels, String topic) {
 
-    try {
-      _duration = duration;
-      _sourceBuffer = dataSource;
-      _rateLimiter = rateLimiter;
-      _clientSocket = aClientSocket;
-      _startTime = startTime;
-      _cosumedTuples = consumedTuples;
-      _maxTuples = maxTupels;
-      _output = new BufferedOutputStream(_clientSocket.getOutputStream());
-      this.start();
-    } catch (IOException e) {
-      System.out.println(e.getMessage());
-    }
+    _duration = duration;
+    _sourceBuffer = dataSource;
+    _rateLimiter = rateLimiter;
+    _kafkaServer = kafkaServer;
+    _startTime = startTime;
+    _cosumedTuples = consumedTuples;
+    _maxTuples = maxTupels;
+    _topic = topic;
+    this.start();
+  }
+
+  private Properties kafkaConfig(){
+      Properties props = new Properties();
+
+      props.put("metadata.broker.list", _kafkaServer);
+      props.put("bootstrap.servers", _kafkaServer);
+      props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+      props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+      props.put("request.required.acks", "1");
+
+      return props;
   }
 
   public void run() {
     try {
 
-      System.out.println("Client " +
-          _clientSocket.getInetAddress() + ":" +
-          _clientSocket.getPort() + " connected. Starting stream..");
-
       String tuple;
       int localTuples = 0;
+      Producer<String, String> producer = new KafkaProducer<>(kafkaConfig());
       while ((tuple = _sourceBuffer.readLine()) != null) {
         _rateLimiter.acquire();
         _cosumedTuples.incrementAndGet();
@@ -54,26 +66,20 @@ public class StreamServer extends Thread {
           break;
         }
 
-        _output.write(tuple.length());
-        _output.write(tuple.getBytes());
+        producer.send(new ProducerRecord<String, String>(_topic,tuple));
 
         localTuples++;
         if (_maxTuples == -1 && System.currentTimeMillis() - _startTime > _duration) {
           break;
         }
       }
-      // Send kill signal
-      _output.write(END_OF_STREAM_SIG);
-      _output.close();
-      System.out.println("Stream ended. Streamed " + localTuples + " tuples.");
+      //Close producer
+      producer.close();
+      System.exit(0);
     } catch (EOFException e) {
       System.out.println("EOF:" + e.getMessage());
     } catch (IOException e) {
       System.out.println("IO:" + e.getMessage());
-    } finally {
-      try {
-        _clientSocket.close();
-      } catch (IOException e) {/*close failed*/}
     }
   }
 }
